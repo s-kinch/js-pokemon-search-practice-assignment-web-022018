0128b6cff5960333df5fc97906d1253ceb1a9fa8
nkalkstein <nkalkstein@gmail.com>
2018-04-10 13:45:18 -0400

    added eventlistener

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+  const input = document.getElementById('pokemon-search-input')
+  input.addEventListener('input', function() {pokemon.filter(poke => poke.name.includes(input.value)).forEach(poke => console.log(poke.name))
+})
+
+
+
+
 })
